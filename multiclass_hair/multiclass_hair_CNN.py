93473fe6a86d133580f3d166b68db05990bb1275
Bruno <brunocalogero@hotmail.com>
2019-01-02 15:58:24 +0000

    multiclass hair gridsearch hyperparameterisation failed because of memory issues, probably one of the batch sizes i tried was too large so wasn't able to retrive 2 days worth of hyperparameterization, this has been logged

---
+++ multiclass_hair/multiclass_hair_CNN.py
@@ -0,0 +1,274 @@
+import numpy
+import os
+import cv2
+
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+
+
+
+from sklearn.model_selection import GridSearchCV
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, MaxPooling2D, BatchNormalization, Dropout, Activation, Flatten, Input
+from keras.wrappers.scikit_learn import KerasClassifier
+from keras.optimizers import Adam
+from keras.callbacks import TensorBoard # only use this after gridsearch selection done well
+
+from sklearn import svm, metrics
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import make_scorer, accuracy_score
+from keras.callbacks import TensorBoard
+
+# user choses grey scale or not, 0 for yes, 1 for no
+grey_scale = 1
+# setting user chosen vars
+images_dir = '../new_dataset'
+labels_filename = 'attribute_list.csv'
+
+def pull_dataset():
+    '''
+    Pulls the full dataset (without outliers).
+    Used as a helper function to obtain full dataset that will be then split to user
+    desired train/validation/test(inference) sets.
+    The examples are shuffled.
+    '''
+
+    # lists keep the order
+    full_dataset = []
+    full_labels = []
+
+    # collect labels
+    df = pd.read_csv(labels_filename, skiprows=1, index_col='file_name')
+    newdf = df['hair_color']
+
+    # collect pre-processed images and sort them to labels
+    for (root, dirs, dat_files) in os.walk('{0}'.format(images_dir)):
+
+        for file in dat_files:
+
+            int_file = int(file[:-4])
+            # removed -1 labeled images for hair colour
+            if df.loc[int_file, 'hair_color'] != -1:
+                # image grayscaling at import
+                img = cv2.imread('{0}/{1}'.format(images_dir, file), grey_scale)
+                # image equalisation (to be inserted if interesting)
+                # rescaling image
+                res = cv2.resize(img, dsize=(128, 128), interpolation=cv2.INTER_LINEAR)
+                # turn to float for zero centering
+                res = res.astype(float)
+                full_dataset.append(res)
+                full_labels.append(int_file)
+
+    # only select rows of interest (none outliers) and only keep 'hair_color' feature to be evaluated (removed -1 labeled images for hair colour)
+    full_labels = newdf.loc[full_labels]
+    full_labels = full_labels.values.tolist()
+
+    # now both of our dataset and labels are ordered
+
+    # numpy array conversion
+    full_dataset = np.array(full_dataset)
+    full_labels = np.array(full_labels)
+
+    print('full dataset of shape:', full_dataset.shape)
+    print('full labels of shape:', full_labels.shape)
+
+    # Reshuffling data (for extra randomness)
+    X_data, Y_data = shuffle(full_dataset, full_labels, random_state=0)
+
+    print('X_data of shape:', X_data.shape)
+    print('Y_data of shape:', Y_data.shape)
+
+    return X_data, Y_data
+
+# Function to create model, required for KerasClassifier
+def create_model(optimizer='adam', learn_rate=0.01, amsgrad=False, activation=tf.nn.leaky_relu):
+
+    channel_1, channel_2, channel_3, num_classes =  32, 16, 8, 10
+    # create model
+    model = Sequential()
+
+    model.add(Conv2D(channel_1, (3, 3), padding='SAME', activation=activation,  input_shape=(128, 128, 3), data_format="channels_last"))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='valid'))
+
+    model.add(Conv2D(channel_2, (3, 3), padding='SAME', activation=activation))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='valid'))
+
+    # model.add(Conv2D(channel_3, (3, 3), padding='SAME', activation=activation))
+    # model.add(BatchNormalization())
+    # model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='valid'))
+
+    model.add(Flatten())
+    model.add(Dense(num_classes))
+
+    optimizer = Adam(lr=learn_rate, amsgrad=amsgrad )
+    # Compile model (sparse cross-entropy used because labels are not one hot encoded)
+    model.compile(loss='sparse_categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+    model.summary()
+    return model
+
+def batch_epoch_hyper(n_folds):
+    '''
+    This function finds the best hyperparameters among those given by the user for batch size and epoch size
+    '''
+
+    print('hyperparameterization: batch_size and epochs')
+    # create model
+    model = KerasClassifier(build_fn=create_model, verbose=0)
+
+    # define the grid search parameters
+    batch_size = [32, 64, 128]
+    epochs = [10, 15, 20]
+    param_grid = dict(batch_size=batch_size, epochs=epochs)
+    grid = GridSearchCV(estimator=model, param_grid=param_grid, cv=n_folds, n_jobs=4)
+    grid_result = grid.fit(X_train, y_train, validation_data=(X_val, y_val))
+    # summarize results
+    print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+    means = grid_result.cv_results_['mean_test_score']
+    stds = grid_result.cv_results_['std_test_score']
+    params = grid_result.cv_results_['params']
+    for mean, stdev, param in zip(means, stds, params):
+        print("%f (%f) with: %r" % (mean, stdev, param))
+
+
+def optimization_func_hyper(n_folds):
+    '''
+    This function finds the best hyperparameters among those given by the user for the chosen optimization function
+    '''
+    print('hyperparameterization: optimization function')
+    # create model
+    model = KerasClassifier(build_fn=create_model, epochs=10, batch_size=64 ,verbose=0)
+
+    # define the grid search parameters
+    optimizer = ['SGD', 'RMSprop', 'Adagrad', 'Adam']
+    param_grid = dict(optimizer=optimizer)
+    grid = GridSearchCV(estimator=model, param_grid=param_grid, cv=n_folds, n_jobs=4)
+    grid_result = grid.fit(X_train, y_train, validation_data=(X_val, y_val))
+    # summarize results
+    print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+    means = grid_result.cv_results_['mean_test_score']
+    stds = grid_result.cv_results_['std_test_score']
+    params = grid_result.cv_results_['params']
+    for mean, stdev, param in zip(means, stds, params):
+        print("%f (%f) with: %r" % (mean, stdev, param))
+
+
+def learning_rate_hyper(n_folds):
+    '''
+    This function finds the best hyperparameters among those given by the user for the chosen learning rate
+    '''
+    print('hyperparameterization: learning_rate / amsgrad')
+    # create model
+    model = KerasClassifier(build_fn=create_model, epochs=10, batch_size=64, verbose=0)
+
+    # define the grid search parameters
+    learn_rate = [0.001, 0.01, 0.1, 0.2, 0.3]
+    amsgrad = [False, True]
+    param_grid = dict(learn_rate=learn_rate, amsgrad=amsgrad)
+    grid = GridSearchCV(estimator=model, param_grid=param_grid, cv=n_folds, n_jobs=4)
+    grid_result = grid.fit(X_train, y_train, validation_data=(X_val, y_val))
+    # summarize results
+    print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+    means = grid_result.cv_results_['mean_test_score']
+    stds = grid_result.cv_results_['std_test_score']
+    params = grid_result.cv_results_['params']
+    for mean, stdev, param in zip(means, stds, params):
+        print("%f (%f) with: %r" % (mean, stdev, param))
+
+
+def activation_hyper(n_folds):
+    '''
+    This function finds the best hyperparameters among those given by the user for the chosen learning rate
+    '''
+
+    print('hyperparameterization: activation function')
+    # create model
+    model = KerasClassifier(build_fn=create_model, epochs=10, batch_size=64, verbose=0)
+
+    # define the grid search parameters
+    activation = ['softmax', 'relu', 'tanh', 'sigmoid', 'linear']
+    param_grid = dict(activation=activation)
+    grid = GridSearchCV(estimator=model, param_grid=param_grid, cv=n_folds, n_jobs=4)
+    grid_result = grid.fit(X_train, y_train, validation_data=(X_val, y_val))
+    # summarize results
+    print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+    means = grid_result.cv_results_['mean_test_score']
+    stds = grid_result.cv_results_['std_test_score']
+    params = grid_result.cv_results_['params']
+    for mean, stdev, param in zip(means, stds, params):
+        print("%f (%f) with: %r" % (mean, stdev, param))
+
+# print('hyperparameterization: number of neurons per layer (TBD)')
+
+
+# import data
+X_data, y_data = pull_dataset()
+# consider one hot encoding
+
+# perform train and test split (random state set to 1 to ensure same distribution accross different sets)
+# this split is obviously case specific! but cross validation allows us to avoid over-fitting so lets make sure we have a validation set ready.
+# Since the dataset is not extrememly large i'll be using a 60/20/20 split, meaning more or less 1000 validation and test examples and 3000 training examples, to be tested: 75/10/15
+# in this case we are a little less concerned since we are evaluating smiles which are present in every case, unlike glasses
+X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.2, random_state=1)
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=1)
+
+# sanity check
+print('X_train of shape:', X_train.shape)
+print('y_train of shape:', y_train.shape)
+print('X_val of shape:', X_val.shape)
+print('y_val of shape:', y_val.shape)
+print('X_test of shape:', X_test.shape)
+print('y_test of shape:', y_test.shape)
+
+# Preprocessing: reshape the image data into rows
+X_train = np.reshape(X_train, (X_train.shape[0], -1))
+X_val = np.reshape(X_val, (X_val.shape[0], -1))
+X_test = np.reshape(X_test, (X_test.shape[0], -1))
+
+# sanity check
+print('X_train of shape:', X_train.shape)
+print('X_val of shape:', X_val.shape)
+print('X_test of shape:', X_test.shape)
+
+# Preprocessing: subtract the mean image
+# first: compute the image mean based on the training data
+mean_image = np.mean(X_train, axis=0)
+print(mean_image[:10]) # print a few of the elements
+# plt.figure(figsize=(4,4))
+# plt.imshow(mean_image.reshape((128,128,3)).astype('uint8')) # visualize the mean image
+# plt.show()
+
+# second: subtract the mean image from train and test data
+X_train -= mean_image
+X_val -= mean_image
+X_test -= mean_image
+
+# sanity check
+print('X_train of shape:', X_train.shape)
+print('X_val of shape:', X_val.shape)
+print('X_test of shape:', X_test.shape)
+
+
+# Preprocessing: reshape back into three channels
+X_train = np.reshape(X_train, (X_train.shape[0], 128, 128, 3))
+X_val = np.reshape(X_val, (X_val.shape[0], 128, 128, 3))
+X_test = np.reshape(X_test, (X_test.shape[0], 128, 128, 3))
+# sanity check
+print('X_train of shape:', X_train.shape)
+print('X_val of shape:', X_val.shape)
+print('X_test of shape:', X_test.shape)
+
+
+# # declaring number of folds for cross_validation
+n_folds = 8
+epoch = 10
+batch_size = 100
+
+
+
+# do inference with tensorboard and best parameters + validation set
+tensorboard = TensorBoard(log_dir="logs/{}".format(time()))
